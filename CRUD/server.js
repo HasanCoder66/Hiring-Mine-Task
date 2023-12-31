5e4e2c149343b6eca197b1c2d7192a72050aa786
HasanCoder66 <hasanashraf874@gmail.com>
2023-12-31 21:47:30 +0500

    db connect

--- CRUD/server.js
+++ CRUD/server.js
@@ -1,10 +1,26 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const PORT = 6500;
+dotenv.config();
+const app = express();
 
-const app = express.Router()
+const connectDb = () => {
+  mongoose
+    .connect(
+      process.env.MONGO_URI
+    )
+    .then(() => {
+      console.log("Mongodb connect");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+app.listen(PORT, () => {
+  console.log(`server listen on this ${PORT}`);
+  connectDb();
+});
 
-app.listen(PORT, ()=> {
-    console.log(`server listen on this ${PORT}`)
-})
-
+// MONGO_URI = 'mongodb+srv://66CoderHasan:c4m1dr0GiwPHLFrB@dummy0.q82kncb.mongodb.net/Dummy0?retryWrites=true&w=majority'
