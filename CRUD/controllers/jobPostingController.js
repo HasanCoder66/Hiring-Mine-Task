638b2d876084f511dcaf86f47b4a9010710ba4cc
HasanCoder66 <hasanashraf874@gmail.com>
2024-01-02 20:32:23 +0500

    task done

--- CRUD/controllers/jobPostingController.js
+++ CRUD/controllers/jobPostingController.js
@@ -1,38 +1,31 @@
 export const jobPosting = async (req, res) => {
   try {
-
     const description = req.body.description;
 
     // Regular expression to match email addresses
     const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
-    
+    // Regular expression to match phone numbers
+    const phoneRegex = /\+\s?\d{3}\s?\d{3}\s?\d{4}/g;
+    const phoneRegex2 = /0\d{9}/g;
     // Extracting email addresses from the description using the regex
     const extractedEmails = description.match(emailRegex);
-    
-    // Regular expression to match phone numbers
-    const phoneRegex = /\b\d{10}\b|\b\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b/g;
-    
-    // Extracting phone numbers from the description using the regex
-    const extractedPhoneNumbers = description.match(phoneRegex);
-    
+
+    var matchedNumbers1 = description.match(phoneRegex);
+    var matchedNumbers2 = description.match(phoneRegex2);
+
     // Logging or using the extracted contact information
     if (extractedEmails) {
       console.log("Extracted Email Addresses:", extractedEmails);
     }
-    
-    if (extractedPhoneNumbers) {
-      console.log("Extracted Phone Numbers:", extractedPhoneNumbers);
+
+    if (matchedNumbers1) {
+      console.log("Extracted Phone Numbers:", matchedNumbers1);
     }
-    
-    // You can use or send these extracted contact information as needed
-    
+    if (matchedNumbers2) {
+      console.log("Extracted Phone Numbers:", matchedNumbers2);
+    }
     //  { $push: { likes: req.body.userId } }
-    
   } catch (error) {
     console.log(error);
   }
 };
-
-// app.post("/post-job", (req, res) => {
-//   // Validate the data if needed
-// });
